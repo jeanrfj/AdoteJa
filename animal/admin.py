f827d47a886a2b7f1abb2d5ea26caa9f3048b037
Jean Augusto Rego <jean_augusto94@live.com>
2023-06-10 18:15:12 -0300

    .

--- animal/admin.py
+++ animal/admin.py
@@ -7,6 +7,7 @@ from . import models
 class FotoInline(admin.TabularInline):
     model = models.Foto
     extra = 1
+    
 
 class AnimalAdmin(admin.ModelAdmin): #ITENS QUE VAO APARECER NO DISPLAY DO ADMIN
     #list_display=['nome','descricao_curta','get_preco_formatado','get_preco_promocional_formatado']
